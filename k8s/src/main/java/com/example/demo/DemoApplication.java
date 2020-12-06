b6079f410695e1f2052925c3e786049d54abbbf4
dukang <15162678717@163.com>
2020-12-06 20:32:53 +0800

    update

--- k8s/src/main/java/com/example/demo/DemoApplication.java
+++ k8s/src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!0000", name);
+		return String.format("Hello %s!99999990000", name);
 	}
 }
